21314b7854a188f9823f23c9a589078593e6a3d9
aleksejqa <73742334+aleksejqa@users.noreply.github.com>
2023-03-23 19:07:29 +0500

    Update +.bat

--- +.bat
+++ +.bat
@@ -1,7 +1,7 @@
 netsh interface ip set address name="wintun" source=static addr=192.168.123.1 mask=255.255.255.0 gateway=192.168.123.1
 
 pause
-route add 91.203.192.107 mask 255.255.255.255 192.168.1.1
+route add {ip-proxy} mask 255.255.255.255 {def getavay}
 route add 0.0.0.0 mask 0.0.0.0 192.168.123.1 METRIC 3 IF 5
 route add -p 0.0.0.0 mask 0.0.0.0 192.168.123.1 METRIC 3 IF 5
 route CHANGE 0.0.0.0 mask 0.0.0.0 192.168.123.1 METRIC 3 IF 5
