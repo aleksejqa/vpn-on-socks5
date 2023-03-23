7f12e972e7ea5a4ead8ce660be5ce48523245327
aleksejqa <73742334+aleksejqa@users.noreply.github.com>
2023-03-23 19:08:03 +0500

    Update -.bat

--- -.bat
+++ -.bat
@@ -1,2 +1,2 @@
-route add 0.0.0.0 MASK 0.0.0.0 192.168.1.1 METRIC 5 IF 3
-route CHANGE 0.0.0.0 MASK 0.0.0.0 192.168.1.1 METRIC 5 IF 3
+route add 0.0.0.0 MASK 0.0.0.0 {def-getavay} METRIC 5 IF 3
+route CHANGE 0.0.0.0 MASK 0.0.0.0 {def-getavay} METRIC 5 IF 3
